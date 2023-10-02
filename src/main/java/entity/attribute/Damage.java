077b4a5ad0fb69da52b1f196f96d5e79b33e096d
debugByPrintln1 <melnikov.n.89@mail.ru>
2023-10-02 13:49:31 +0300

    hotfix 2.0

--- src/main/java/entity/attribute/Damage.java
+++ src/main/java/entity/attribute/Damage.java
@@ -8,6 +8,9 @@ public class Damage {
     private final int maxDamage;
 
     public Damage(int minDamage, int maxDamage) {
+        if(minDamage < 0 || maxDamage < 0){
+            throw new IllegalArgumentException("Damage can't be a negative integer.");
+        }
         if (minDamage > maxDamage){
             this.minDamage = maxDamage;
             this.maxDamage = minDamage;
